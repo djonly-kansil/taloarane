9ff477417cec5d9de368e7cb51fbb7c7d159bd53
djonly-kansil <djonlykansil@gmail.com>
2025-04-24 22:29:33 +0800

    update

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,15 @@
-import { supabase } from "../supabase-config.js";
-
 const sliderTrack = document.getElementById("slider-track");
 const totalSlides = 10;
 
-(async () => {
+(() => {
 	const imageUrls = [];
 	
 	for (let i = 1; i <= totalSlides; i++) {
-		const fileName = `slide${i}.jpg`;
-		
-		// Ambil URL publik dari Supabase
-		const { data, error } = supabase.storage
-			.from("taloarane")
-			.getPublicUrl(fileName);
-		
-		if (error) {
-			console.warn(`❌ Gagal ambil URL untuk ${fileName}`, error.message);
-			continue;
-		}
-		
-		imageUrls.push(data.publicUrl);
+		const fileName = `/images/foto${i}.jpg`;
+		imageUrls.push(fileName);
 	}
 	
-	// Gandakan untuk efek slide looping (seperti sebelumnya)
+	// Gandakan untuk efek slide looping
 	const allImages = [...imageUrls, ...imageUrls];
 	
 	allImages.forEach((url) => {
